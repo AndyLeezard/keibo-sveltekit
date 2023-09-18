d5a6935102fb0e5a32beb45f8bc1d9ca1b9e16ed
Andy Lee <a.lee.intl@gmail.com>
2023-09-19 01:34:09 +0200

    Add more global types

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -1,5 +1,13 @@
 declare global {
 	type Optional<T> = T | undefined;
+	type ValueOf<T> = T[keyof T]
+  type PartiallyOptional<T, K extends keyof T> = Omit<T, K> &
+    Partial<Pick<T, K>>
+  type PartiallyRequired<T, K extends keyof T> = Omit<T, K> &
+    Required<Pick<T, K>>
+  type RequiredAttributes<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+  }
 }
 
 export type module = unknown;
