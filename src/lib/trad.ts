e8c3dd8b4e0621a710e300b6f419e61ebc0e0e7c
Andy Lee <a.lee.intl@gmail.com>
2023-09-19 01:34:41 +0200

    More utility functions

---
+++ src/lib/trad.ts
@@ -0,0 +1,22 @@
+import { language } from '@inlang/sdk-js';
+
+export const i18n = {
+	defaultLocale: 'en',
+	locales: ['en', 'fr', 'ko', 'de']
+} as const;
+
+export type Locale = (typeof i18n)['locales'][number];
+export type PDictionary = PartiallyRequired<
+	Partial<Record<Locale, string>>,
+	(typeof i18n)['defaultLocale']
+>;
+
+/** Takes a dictionary and conditionally return the corresponding output */
+export const t = (dictionary: PDictionary | string | undefined): string => {
+	if (!dictionary) return '';
+	if (typeof dictionary === 'string') return dictionary;
+	if (Object.prototype.hasOwnProperty.call(dictionary, language)) {
+		return dictionary[language as keyof PDictionary] as string;
+	}
+	return dictionary[i18n.defaultLocale] as string;
+};
