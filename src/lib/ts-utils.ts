8152dd730c680153ea8b4830ee54351ae0371930
Andy Lee <a.lee.intl@gmail.com>
2023-12-13 00:09:57 +0100

    utils/format date

--- src/lib/ts-utils.ts
+++ src/lib/ts-utils.ts
@@ -30,3 +30,10 @@ export const normalizeStr = (str: string, unifyCase?: 'uppercase' | 'lowercase')
   return res;
 };
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+/** Precision-level: seconds */
+export const formatDate = (date: Date) => {
+  const pad = (num: number) => num.toString().padStart(2, '0');
+  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(
+    date.getHours()
+  )}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+};
